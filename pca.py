901cb3448d6e83b8f331bb022c53eccb600be009
Luca <48834642+IlNin@users.noreply.github.com>
2021-02-19 12:35:38 +0100

    Add files via upload

---
+++ pca.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask import make_response
+from flask_cors import CORS, cross_origin
+
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+from matplotlib import pyplot as plt
+
+app = Flask(__name__)
+#app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+#app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin'
+
+CORS(app)
+#CORS(app, resources={r"/pca": {"origins": "http://localhost:8000"}})
+
+@app.route("/")
+def home():
+    return "hi"
+@app.route("/index")
+
+@app.route('/pca', methods=['POST'])
+#@cross_origin(origin='localhost',headers=['Access-Control-Allow-Origin', 'Content-Type'])
+def pca():
+   if request.method == 'POST':
+        # Reads and parses the data
+        print("POST request acknowledged")
+        data = request.get_json(force=True)
+        print("Data read and parsed!")
+        
+        # Prepares the data for the PCA process
+        dataForPCA = []
+        for element in data['value']:
+            dataForPCA.append([element['championships'], element['points'], element['poles'], element['wins']])
+        
+        # Transforms the data
+        d_std = preprocessing.StandardScaler().fit_transform(dataForPCA)
+        
+        # Chooses number of PCA components
+        pca = PCA(n_components=2)
+
+        # Applies PCA
+        dpca = pca.fit_transform(d_std)     
+        print(dpca)
+        
+        # Sends the new data back
+        response = jsonify(dpca.tolist())
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
